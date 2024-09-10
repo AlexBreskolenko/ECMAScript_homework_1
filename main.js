2d25b87306a85aa28386f471e68dac54c95ca935
Alex <89929071+AlexBreskolenko@users.noreply.github.com>
2024-09-10 20:14:36 +0300

    Task 2 completed

--- main.js
+++ main.js
@@ -1,2 +1,25 @@
+// ********************Task 1
 const arr = [1, 5, 7, 9];
 console.log(`Минимальное число в массиве ${Math.min(...arr)}`);
+// ********************Task 2
+function createCounter() {
+  let count = 0;
+  return {
+    increment: () => {
+      count++;
+    },
+    decrement: () => {
+      count--;
+    },
+    result: () => {
+      return console.log(`Результат ${count}`);
+    },
+  };
+}
+
+let counter = createCounter();
+counter.increment();
+counter.increment();
+counter.result();
+counter.decrement();
+counter.result();
